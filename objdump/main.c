ccc0fe6f11fa897d63508d796832ab972d51156a
broggi_t <broggi_t@epitech.eu>
2015-04-26 00:53:57 +0200

    objdump is now multi-arguments

--- objdump/main.c
+++ objdump/main.c
@@ -10,15 +10,30 @@
 
 #include "objdump.h"
 
+static int	args(int const ac,
+		     char const *const *const av,
+		     t_elf *const elf)
+{
+  int		i;
+
+  i = 0;
+  while (++i < ac)
+    if (!parse_file(av[i], elf)
+	|| !write_header(elf)
+	|| !write_data(elf))
+      return (0);
+  return (1);
+}
+
 int	main(int const ac, char const *const *const av)
 {
   t_elf	elf;
 
   if ((ac < 2
-       && !parse_file("a.out", &elf))
-      || !parse_file(av[1], &elf)
-      || !write_header(&elf)
-      || !write_data(&elf))
+       && (!parse_file("a.out", &elf)
+	   || !write_header(&elf)
+	   || !write_data(&elf)))
+      || !args(ac, av, &elf))
     return (EXIT_FAILURE);
   return (EXIT_SUCCESS);
 }
